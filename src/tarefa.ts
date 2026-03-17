0c747fe5ce30f4313e80aca55409061037b61376
juamorim201427-droid <juamorim201427@gmail.com>
2026-03-16 21:20:42 -0300

    Update tarefa.ts

--- src/tarefa.ts
+++ src/tarefa.ts
@@ -1,77 +1,32 @@
-import express, {Request, Response} from "express";
-
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-
-type Tarefa = {
-    id: number,
-    titulo: string,
-    concluida: boolean
+{
+  "name": "tsi-api-a1",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "dev" : "ts-node src/server.ts",
+    "build": "tsc",
+    "start": "node dist/server.js"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/marcoweb/tsi-api-a1.git"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "type": "commonjs",
+  "bugs": {
+    "url": "https://github.com/marcoweb/tsi-api-a1/issues"
+  },
+  "homepage": "https://github.com/marcoweb/tsi-api-a1#readme",
+  "devDependencies": {
+    "@types/express": "^5.0.6",
+    "@types/node": "^25.3.3",
+    "ts-node": "^10.9.2",
+    "typescript": "^5.9.3"
+  },
+  "dependencies": {
+    "express": "^5.2.1"
+  }
 }
-
-let tarefas: Tarefa[] = [
-    {id: 1, titulo: "Estudar Express", concluida: false},
-    {id: 2, titulo: "Estudar para Prova", concluida: false}
-];
-
-app.get("/tarefas", (req: Request, res: Response) =>{
-    res.json(tarefas);
-});
-
-app.get("/tarefas/:id", (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-
-    const tarefa = tarefas.find(t => t.id == id);
-
-    if(!tarefa) {
-        return res.status(404).json({erro: "Tarefa não encontrada"});
-    }
-
-    res.json(tarefa);
-});
-
-app.post("/tarefas", (req: Request, res: Response) => {
-    const {titulo} = req.body;
-
-    const novaTarefa: Tarefa = {
-        id: tarefas.length + 1,
-        titulo: titulo,
-        concluida: false
-    }
-
-    tarefas.push(novaTarefa);
-    res.status(201).json(novaTarefa);
-});
-
-app.put("/tarefas/:id", (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-
-    const tarefa = tarefas.find(t => t.id == id);
-
-    if(!tarefa) {
-        return res.status(404).json({erro : "Tarefa não encontrada"});
-    }
-
-    const {titulo, concluida} = req.body;
-
-    tarefa.titulo = titulo ?? tarefa.titulo,
-    tarefa.concluida = concluida ?? tarefa.concluida
-
-    res.json(tarefa);
-});
-
-app.delete("/tarefas/:id", (req: Request, res: Response) => {
-    const id = Number(req.params.id);
-
-    tarefas = tarefas.filter(t => t.id !== id);
-
-    res.json({
-        mensagem: "Tarefa Removida"
-    });
-});
-
-app.listen(PORT, () => {
-    console.log(`Servidor executando em localhost:${PORT}`);
-});
